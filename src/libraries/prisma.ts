77c97e550fa1f25d155eef4c5b6b04b7a95ba4b3
Jonathan Zero <axis@axis.moe>
2023-12-08 01:08:29 -0500

    fix

--- src/libraries/prisma.ts
+++ src/libraries/prisma.ts
@@ -3,11 +3,5 @@ import { PrismaClient } from "@prisma/client";
 export let prisma: PrismaClient;
 
 export const prismaLibrary = (): void => {
-  prisma ??= new PrismaClient({
-    datasources: {
-      db: {
-        url: `postgresql://cswap:cswap@localhost:5432/cswap`,
-      },
-    },
-  });
+  prisma ??= new PrismaClient();
 };
